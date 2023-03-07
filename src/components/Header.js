e9493e4adc543cb1cfc7be14346597fc5f09efb1
Tayan <bostybaev@list.ru>
2023-03-07 23:36:04 +0600

    fix Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,52 +1,55 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import { AppContext } from "../App";
 
 const Header = (props) => {
-  const {cartItems} = React.useContext(AppContext)
+  const { cartItems } = React.useContext(AppContext);
 
-  const totalPrice = cartItems.reduce((sum, ob) => ob.price + sum, 0)
+  const totalPrice = cartItems.reduce((sum, ob) => ob.price + sum, 0);
 
   return (
-    <header className="d-flex justify-between align-center p-40">
-      <Link to="/">
-        <div className="d-flex align-center">
-          <img width={40} height={40} src="/img/logo.png" alt="logotype" />
-          <div className="headerInfo">
-            <h3 className="text-uppercase">React Snickers</h3>
-            <p>Магазин лучших кроссовок</p>
+    <>
+      <header className="d-flex justify-between align-center p-40">
+        <Link to="/">
+          <div className="d-flex align-center">
+            <img width={40} height={40} src="/img/logo.png" alt="logotype" />
+            <div className="headerInfo">
+              <h3 className="text-uppercase">React Snickers</h3>
+              <p>Магазин лучших кроссовок</p>
+            </div>
           </div>
-        </div>
-      </Link>
+        </Link>
 
-      <ul className="d-flex">
-        <li className="mr-30 cu-p">
-          <img
-            onClick={props.onClickCart}
-            width={18}
-            height={18}
-            src="/img/cart.svg"
-            alt="cart"
-          />
-          <span>{totalPrice}тг</span>
-        </li>
-        <li className="mr-20 cu-p">
-          <Link to="/favorites">
+        <ul className="d-flex">
+          <li className="mr-30 cu-p">
             <img
+              onClick={props.onClickCart}
               width={18}
               height={18}
-              src="/img/favorite.svg"
-              alt="favorite"
+              src="/img/cart.svg"
+              alt="cart"
             />
-          </Link>
-        </li>
-        <li>
-          <Link to="/orders">
-           <img width={18} height={18} src="/img/user.svg" alt="user" />
-          </Link> 
-        </li>
-      </ul>
-    </header>
+            <span>{totalPrice}тг</span>
+          </li>
+          <li className="mr-20 cu-p">
+            <Link to="/favorites">
+              <img
+                width={18}
+                height={18}
+                src="/img/favorite.svg"
+                alt="favorite"
+              />
+            </Link>
+          </li>
+          <li>
+            <Link to="/orders">
+              <img width={18} height={18} src="/img/user.svg" alt="user" />
+            </Link>
+          </li>
+        </ul>
+      </header>
+      <Outlet />
+    </>
   );
 };
 
